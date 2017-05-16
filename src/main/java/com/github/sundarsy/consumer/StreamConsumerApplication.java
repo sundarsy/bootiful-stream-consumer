5c021ddbcef2d7417402f0899d761e4b6628fa17
sundarsy <sundarsy@gmail.com>
2017-05-16 02:01:08 -0500

    First Commit

---
+++ src/main/java/com/github/sundarsy/consumer/StreamConsumerApplication.java
@@ -0,0 +1,26 @@
+package com.github.sundarsy.consumer;
+
+import org.apache.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+
+
+@SpringBootApplication
+@EnableBinding(Sink.class)
+public class StreamConsumerApplication {
+	protected Logger logger = Logger.getLogger(StreamConsumerApplication.class.getName());
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamConsumerApplication.class, args);
+	}
+	
+	@StreamListener(Sink.INPUT)
+	public void loggerSink(String message) {
+		logger.info("Received: " + message);
+	}
+ 
+}
