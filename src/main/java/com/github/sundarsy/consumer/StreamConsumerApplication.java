dcf9fc9b08e10ba645acabf23282b188ef123da1
zeagord <sundarsy@gmail.com>
2018-06-18 15:02:53 +0700

    Switches to Springboot2

--- src/main/java/com/github/sundarsy/consumer/StreamConsumerApplication.java
+++ src/main/java/com/github/sundarsy/consumer/StreamConsumerApplication.java
@@ -1,6 +1,7 @@
 package com.github.sundarsy.consumer;
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
@@ -11,13 +12,13 @@ import org.springframework.cloud.stream.messaging.Sink;
 @SpringBootApplication
 @EnableBinding(Sink.class)
 public class StreamConsumerApplication {
-	protected Logger logger = Logger.getLogger(StreamConsumerApplication.class.getName());
+	protected Logger logger = LoggerFactory.getLogger(StreamConsumerApplication.class.getName());
 
 
 	public static void main(String[] args) {
 		SpringApplication.run(StreamConsumerApplication.class, args);
 	}
-	
+
 	@StreamListener(Sink.INPUT)
 	public void loggerSink(String message) {
 		logger.info("Received: " + message);
